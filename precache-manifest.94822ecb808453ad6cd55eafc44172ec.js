903d47981c39b77eff86b0159048daa7974441e3
michael oluwadamilare <dareflix95@gmail.com>
2020-06-11 03:18:37 +0100

    Updates

---
+++ precache-manifest.94822ecb808453ad6cd55eafc44172ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2813e059da1a545f57f6d36b4e284f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "11344871630b1a0c4681",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "0a964ed7e2e548bb0b95",
+    "url": "/robofriends/static/css/main.21c108c0.chunk.css"
+  },
+  {
+    "revision": "11344871630b1a0c4681",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a964ed7e2e548bb0b95",
+    "url": "/robofriends/static/js/main.c246dd14.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
